a428e1daf4475634f6892485f1a1fd12263b54a7
flekpy <flekpy@gmail.com>
2023-10-21 13:06:22 +0800

    fish command line change

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
+# lsd сторонняя программа, для красивого вывода содержимого директории
+alias ls='lsd'
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set --g -x fish_greeting ''
+     set --g -x fish_greeting ''
     starship init fish | source
 end
